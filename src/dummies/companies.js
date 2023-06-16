654997b93b17f3bb6d37d6e81c17c37ef1325ad7
David Umoru <umorudavido@gmail.com>
2023-06-16 13:17:08 +0100

    Export an array of company objects

---
+++ src/dummies/companies.js
@@ -0,0 +1,54 @@
+const companies = [
+    {
+        id: "1",
+        name: "Microsoft",
+        address: "Redmond, Washington, USA",
+        cacNo: "MSFT123456",
+        contactEmail: "contact@microsoft.com",
+        website: "microsoft.com",
+        contactPhone: "1234567890",
+        logo: "https://example.com/microsoft_logo.png"
+    },
+    {
+        id: "2",
+        name: "Amazon",
+        address: "Seattle, Washington, USA",
+        cacNo: "AMZN987654",
+        contactEmail: "contact@amazon.com",
+        website: "amazon.com",
+        contactPhone: "0987654321",
+        logo: "https://example.com/amazon_logo.png"
+    },
+    {
+        id: "3",
+        name: "Apple",
+        address: "Cupertino, California, USA",
+        cacNo: "AAPL345678",
+        contactEmail: "contact@apple.com",
+        website: "apple.com",
+        contactPhone: "9876543210",
+        logo: "https://example.com/apple_logo.png"
+    },
+    {
+        id: "4",
+        name: "Netflix",
+        address: "Los Gatos, California, USA",
+        cacNo: "NFLX567890",
+        contactEmail: "contact@netflix.com",
+        website: "netflix.com",
+        contactPhone: "0123456789",
+        logo: "https://example.com/netflix_logo.png"
+    },
+    {
+        id: "5",
+        name: "Google",
+        address: "Mountain View, California, USA",
+        cacNo: "GOOG234567",
+        contactEmail: "contact@google.com",
+        website: "google.com",
+        contactPhone: "4567890123",
+        logo: "https://example.com/google_logo.png"
+    }
+];
+
+module.exports = companies
