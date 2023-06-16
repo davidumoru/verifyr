89a884baee321b7f3cb9e6eb297853c974ba657f
David Umoru <umorudavido@gmail.com>
2023-06-16 13:15:58 +0100

    Export an array of user objects

---
+++ src/dummies/users.js
@@ -0,0 +1,103 @@
+const users = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      phone: "1234567890",
+      email: "johndoe@gmail.com",
+      password: "password1",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      phone: "9876543210",
+      email: "janesmith@gmail.com",
+      password: "password2",
+    },
+    {
+      firstName: "Michael",
+      lastName: "Johnson",
+      phone: "5555555555",
+      email: "michaeljohnson@gmail.com",
+      password: "password3",
+    },
+    {
+      firstName: "Emily",
+      lastName: "Brown",
+      phone: "9999999999",
+      email: "emilybrown@gmail.com",
+      password: "password4",
+    },
+    {
+      firstName: "David",
+      lastName: "Lee",
+      phone: "1111111111",
+      email: "davidlee@gmail.com",
+      password: "password5",
+    },
+    {
+      firstName: "Sarah",
+      lastName: "Wilson",
+      phone: "2222222222",
+      email: "sarahwilson@gmail.com",
+      password: "password6",
+    },
+    {
+      firstName: "Christopher",
+      lastName: "Taylor",
+      phone: "3333333333",
+      email: "christophertaylor@gmail.com",
+      password: "password7",
+    },
+    {
+      firstName: "Olivia",
+      lastName: "Miller",
+      phone: "4444444444",
+      email: "oliviamiller@gmail.com",
+      password: "password8",
+    },
+    {
+      firstName: "Daniel",
+      lastName: "Anderson",
+      phone: "6666666666",
+      email: "danielanderson@gmail.com",
+      password: "password9",
+    },
+    {
+      firstName: "Sophia",
+      lastName: "Wilson",
+      phone: "7777777777",
+      email: "sophiawilson@gmail.com",
+      password: "password10",
+    },
+    {
+      firstName: "Matthew",
+      lastName: "Thomas",
+      phone: "8888888888",
+      email: "matthewthomas@gmail.com",
+      password: "password11",
+    },
+    {
+      firstName: "Ava",
+      lastName: "Martinez",
+      phone: "5554443333",
+      email: "avamartinez@gmail.com",
+      password: "password12",
+    },
+    {
+      firstName: "Andrew",
+      lastName: "Harris",
+      phone: "2224446666",
+      email: "andrewharris@gmail.com",
+      password: "password13",
+    },
+    {
+      firstName: "Isabella",
+      lastName: "Clark",
+      phone: "1113335555",
+      email: "isabellaclark@gmail.com",
+      password: "password14",
+    },
+  ];
+  
+  module.exports = users;
+  
