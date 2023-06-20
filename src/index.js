9f0847585bfaf9a616ef628859561b8d1cc5b0dc
David Umoru <umorudavido@gmail.com>
2023-06-20 13:48:58 +0100

    Implement function to establish MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express = require("express")
-const app = express()
-const PORT = 5003 
+const express = require("express");
+const dotenv = require("dotenv");
 
-const usersRouter = require("./routes/users.routes")
-const paymentRouter = require("./routes/payment.routes")
-const company = require("./routes/company.routes")
+const usersRouter = require("./routes/users.routes");
+const paymentRouter = require("./routes/payment.routes");
+const company = require("./routes/company.routes");
+const connectDB = require("./configs/database");
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5006;
+
+connectDB(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use("/users", userRouter);
@@ -14,9 +19,9 @@ app.use("/getUsers", usersRouter);
 app.use("/registerCompany", company);
 
 app.get("/", (req, res) => {
-    res.status(200).json({message: "I am running"});
+  res.status(200).json({ message: "I am running" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running with speed at port ${PORT}`);
-})
+  console.log(`Server is running with speed at port ${PORT}`);
+});
