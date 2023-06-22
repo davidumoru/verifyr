ef17634e16bc4ad8ca1d3269697cc3c5a2db834a
David Umoru <umorudavido@gmail.com>
2023-06-22 02:42:34 +0100

    Refactor staffSchema definition

---
+++ src/models/staff.models.js
@@ -0,0 +1,11 @@
+const staffSchema = {
+    firstName: "String",
+    lastName: "String",
+    phone: "String",
+    email: "String",
+    employeeId: "String",
+    companyRole: "String",
+    dateofBirth: "String",
+    company: "String",
+    createdAt: "String",
+};
