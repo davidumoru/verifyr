a66cfb67da79547a80923d3f6bdfd8d596965eaa
David Umoru <umorudavido@gmail.com>
2023-06-22 02:45:36 +0100

    Refactored payment routes: import and route handler updates

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,15 +1,8 @@
-const express = require("express")
-const paymentsControl = require("../controllers/payments.controllers")
+const express = require("express");
+const paymentControllers = require("../controllers/payment.controllers");
 
-const router = express.Router ()
+const router = express.Router();
 
-router.post("/pay", paymentsControl.userController)
+router.post("/pay", paymentControllers.paymentControllers);
 
-router.get("/receipt", (req, res) => {
-    console.log(req.body)
-    res.status(200).json({
-        message: "This is your receipt"
-    })
-})
- 
-module.exports = router
+module.exports = router;
