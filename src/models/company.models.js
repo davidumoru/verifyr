2c5d44ac440316b48e535317b6303eb1ccf90dde
David Umoru <umorudavido@gmail.com>
2023-06-22 02:40:51 +0100

    Refactor companySchema for MongoDB using Mongoose

--- src/models/company.models.js
+++ src/models/company.models.js
@@ -1,9 +1,38 @@
-const companySchema = {
-    name: "string", //required
-    address: "string",
-    cacNo: "string",
-    contactEmail: "string",
-    website: "string",
-    contactPhone: "string",
-    logo: "string"
-}
+const { mongoose } = require("mongoose");
+
+const companySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  regNo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contactEmail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  contactPhone: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  },
+});
+
+module.exports = mongoose.model("company", companySchema);
