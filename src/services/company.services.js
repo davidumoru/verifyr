c35041ce1d2826ac4b8cf33411c35d666b7af8d2
David Umoru <umorudavido@gmail.com>
2023-07-11 03:35:21 +0100

    Update company and staff controllers

--- src/services/company.services.js
+++ src/services/company.services.js
@@ -1,66 +1,154 @@
-const Company = require("../models/company.models");
-const responses = require("../utils/response");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Company = require("../models/company.model")
+const Staff = require('../models/staff.model');
+const responses = require("../utils/response")
 
-const createCompany = async (payload) => {
-  const { name, contactEmail, regNo } = payload;
+async function createCompany (payload) {
+    /**
+     * Check if name and email and regNo [are already registered
+     * Add the company to db
+     */
+    // const {name, contactEmail, regNo} = payload;
+    const foundName = await Company.findOne({name: payload.name});
+    if(foundName) {
+        return responses.buildFailureResponse("Company Name Already registered", 400)
+    }
 
-  const foundName = await Company.findOne({ name: name });
-  if (foundName) {
-    return responses.buildFailureResponse("Company name already exists", 400);
-  }
+    const foundEmail = await Company.findOne({contactEmail: payload.contactEmail});
+    if(foundEmail) {
+        return responses.buildFailureResponse("Company email already registered", 400)
+    }
 
-  const foundContactEmail = await Company.findOne({
-    contactEmail: contactEmail,
-  });
-  if (foundContactEmail) {
-    return responses.buildFailureResponse("Contact email already exists", 400);
-  }
+    // now that we have validated our data, let us now create the db
 
-  const foundRegNo = await Company.findOne({ regNo: regNo });
-  if (foundRegNo) {
-    return responses.buildFailureResponse(
-      "Registration number already exists",
-      400
-    );
-  }
+    // const newCompany = new Company(payload);
+    // await newCompany.save();
 
-  const newCompany = await Company.create(payload);
-  return responses.buildSuccessResponse(
-    "Company created successfully",
-    201,
-    newCompany
-  );
-};
+    // OR
+
+    const newCompany = await Company.create(payload)
+    return responses.buildSuccessResponse("Company created successfully", 201, newCompany)
+
+}
+
+async function createAdmin (payload) {
+
+
+    // const foundEmail = await staff.findOne({email: payload.email});
+    // if(foundEmail) {
+    //     return {
+    //         message: "Staff email already registered",
+    //         statusCode: 400,
+    //         status: "failure"
+    //     }
+    // }
+    
+    // const foundPhone = await staff.findOne({phone: payload.phone});
+    // if(foundPhone) {
+    //     return {
+    //         message: "Staff phone already registered",
+    //         statusCode: 400,
+    //         status: "failure"
+    //     }
+    // }
+
+    const foundEmailOrPhone = await Staff.findOne({$or: [
+      {email: payload.email}, 
+      {phone: payload.phone}
+    ]})
+    if(foundEmailOrPhone) {
+        return responses.buildFailureResponse("Staff phone or email duplicate", 400)
+    }
+    // hashing the password here
+    const saltRounds = 10;
+    const generatedSalt = await bcrypt.genSalt(saltRounds)
+    
+    const hashedPassword = await bcrypt.hash(payload.password, generatedSalt)
+    
+
+    payload.password = hashedPassword
+    payload.role = "admin"
+
+    const savedStaff = await Staff.create(payload)
+    return responses.buildSuccessResponse("Staff created successfully", 201, savedStaff)
+    
+}
+
+async function createStaff (payload) {
+
+    const foundEmailOrPhone = await Staff.findOne({$or: [
+      {email: payload.email}, 
+      {phone: payload.phone}
+    ]})
+    if(foundEmailOrPhone) {
+      return {
+            message: "Staff phone or email duplicate",
+            statusCode: 400,
+            status: "failure"
+        }
+    }
+    // hashing the password here
+    const saltRounds = 10;
+    const generatedSalt = await bcrypt.genSalt(saltRounds)
+    
+    const hashedPassword = await bcrypt.hash(payload.password, generatedSalt)
+    
+
+    payload.password = hashedPassword
+    payload.role = "user"
+
+    const savedStaff = await Staff.create(payload)
+    return {
+            message: "Staff created successfully",
+            statusCode: 201,
+            status: "success",
+            data: savedStaff
+        }
+    
+}
 
 const login = async (payload) => {
-  try {
-    const foundUser = await staff.findOne({ email: payload.email });
-    if (!foundUser) {
-      return responses.buildFailureResponse("User does not exist", 404);
-    }
-    const foundPassword = await bcrypt.compare(
-      payload.password,
-      foundUser.password
-    );
-    if (!foundPassword) {
-      return responses.buildFailureResponse("Password is incorrect", 400);
-    }
-    const token = jwt.sign(
-      {
-        email: foundUser.email,
-        firstName: foundUser.firstName,
-        lastName: foundUser.lastName,
-        role: foundUser.role,
-      },
-      process.env.SECRET_KEY,
-      { expiresIn: "30" }
-    );
-    foundUser.accessToken = token;
-    return responses.buildSuccessResponse("Login successful", 200, foundUser);
-  } catch (error) {
-    return responses.buildFailureResponse("Something went wrong", 500);
-  }
-};
+    try {
+        const foundUser = await Staff.findOne({ email: payload.email}).lean()
+        if(!foundUser) {
+            return {
+                message: "User not found",
+                status: "failure",
+                statusCode: 400
+            }
+        }
+        if(foundUser.role !== "admin") {
+            return responses.buildFailureResponse("Only Admins Allowed", 403)
+        }
+        const foundPassword = await bcrypt.compare(payload.password, foundUser.password)
+        if(!foundPassword) {
+            return {
+                message: "Password incorrect",
+                status: "failure",
+                statusCode: 403
+            }
+        }
 
-module.exports = { createCompany, login };
+        const token = jwt.sign({email: foundUser.email, firstName: foundUser.firstName, role: foundUser.role, _id: foundUser._id}, process.env.JWT_SECRET, {
+              expiresIn: '30d'
+       })
+       foundUser.accessToken = token
+       return responses.buildSuccessResponse("Login successful", 200, foundUser)
+    } catch (error) {
+        return {
+            message: "Unable to Login",
+            status: "failure",
+            statusCode: 500
+        }
+    }
+}
+
+
+
+module.exports = {
+    createCompany,
+    createAdmin,
+    login,
+    createStaff
+}
