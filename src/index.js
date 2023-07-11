dc73dd010421695631acedd65de756861384b434
David Umoru <umorudavido@gmail.com>
2023-07-11 03:27:39 +0100

    Update routes and rate limiting configuration

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express')
+const dotenv = require('dotenv')
+const rateLimit = require('express-rate-limit')
 
-const usersRouter = require("./routes/users.routes");
-const paymentRouter = require("./routes/payment.routes");
-const companyRouter = require("./routes/company.routes");
-const connectDB = require("./configs/database");
+const usersRouter = require("./routes/users.routes")
+const paymentRouter = require("./routes/payment.routes")
+const companiesRouter = require("./routes/companies.routes")
+const connectDB = require("./configs/database")
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5006;
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3001
 
-connectDB(process.env.MONGO_URI);
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
-app.use(express.json());
-app.use("/users", usersRouter);
-app.use("/payment", paymentRouter);
-app.use("/company", companyRouter);
+// connecting to db here
+connectDB(process.env.MONGO_URI)
+
+app.use(express.json())
+app.use(limiter)
+app.use('/users', usersRouter)
+app.use('/payments', paymentRouter)
+app.use('/companies', companiesRouter)
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "I am running" });
-});
+    res.status(200).json({
+        message: "I running"
+    })
+})
+
+
 
 app.listen(PORT, () => {
-  console.log(`Server is running with speed at port ${PORT}`);
-});
+    console.log(`Server is running with speed at port ${PORT}`);
+})
