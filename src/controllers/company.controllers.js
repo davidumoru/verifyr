8890bb14232ddde5167ea054ac2f9a19f405a7e9
David Umoru <umorudavido@gmail.com>
2023-07-11 03:30:45 +0100

    Add createAdmin function

--- src/controllers/company.controllers.js
+++ src/controllers/company.controllers.js
@@ -5,9 +5,14 @@ async function createAccount(req, res) {
   res.status(response.status).json(response);
 }
 
+const createAdmin = async (req, res) => {
+  const data = await companyServices.createAdmin(req.body)
+  res.status(data.statusCode).json(data)
+};
+
 async function login(req, res) {
   const response = await companyServices.login(req.body);
   res.status(response.status).json(response);
 }
 
-module.exports = { createAccount, login };
+module.exports = { createAccount, createAdmin, login };
