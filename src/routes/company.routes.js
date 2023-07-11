e8f4e028306d0b3bff78589e63682af41b0b8986
David Umoru <umorudavido@gmail.com>
2023-07-11 03:34:02 +0100

    Add authentication middleware to createAdmin route

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const companyControllers = require("../controllers/company.controllers");
+const authMiddleware = require("../middlewares/auth")
 
 const router = express.Router();
 
 router.post("/createAccount", companyControllers.createAccount);
+router.post('/createAdmin', authMiddleware.authenticate, companyController.createAdmin);
 router.post("/login", companyControllers.login);
 
 module.exports = router;
