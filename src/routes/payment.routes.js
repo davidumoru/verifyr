dc4ef654a9104f20e29571625ff3aa47dd148ebd
David Umoru <umorudavido@gmail.com>
2023-07-11 03:24:39 +0100

    Remove payment

--- src/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const paymentControllers = require("../controllers/payment.controllers");
-
-const router = express.Router();
-
-router.post("/pay", paymentControllers.paymentControllers);
-
-module.exports = router;
