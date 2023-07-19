c0c2e5d19b4c9608eb21adaf9e0ced8980d4638f
David Umoru <umorudavido@gmail.com>
2023-07-19 13:43:40 +0100

    Display README when server starts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const rateLimit = require('express-rate-limit')
+const fs = require('fs');
+const { marked } = require('marked'); 
 
 const companiesRouter = require("./routes/company.routes")
 const connectDB = require("./configs/database")
@@ -23,13 +25,19 @@ app.use(express.json())
 app.use(limiter)
 app.use('/companies', companiesRouter)
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "I running"
-    })
-})
-
-
+app.get('/', (req, res) => {
+// Read the contents of README.md and parse it as markdown
+fs.readFile('README.md', 'utf8', (err, data) => {
+    if (err) {
+    res.status(500).send('Error reading README.md');
+    } else {
+    // Parse markdown to HTML using marked
+    const htmlContent = marked(data);
+    // Send the HTML content as the response
+    res.send(htmlContent);
+    }
+});
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running with speed at port ${PORT}`);
