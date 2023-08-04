9094799ef3412db2f456944da4a1b6780e6b6242
David Umoru <umorudavido@gmail.com>
2023-08-04 09:10:20 +0100

    Define paystack webhook controller

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -10,4 +10,17 @@ const initiatePayment = async (req, res) => {
   }
 };
 
-module.exports = { initiatePayment }
+// paystack webhook controller
+const paystackWebhook = async (req, res) => {
+  try {
+    const response = await paymentServices.paystackWebhook(req.body);
+    res.status(response.statusCode).json(response);
+  } catch (error) {
+    return res.status(5000).json({
+      message: "Unable to resolve",
+      status: "failure",
+    });
+  }
+};
+
+module.exports = { initiatePayment, paystackWebhook };
