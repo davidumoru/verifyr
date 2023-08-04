7d43cfb8a68a9a8f4f8d7db53f6cc7958101ea36
David Umoru <umorudavido@gmail.com>
2023-08-04 16:09:48 +0100

    Add error handling for each controller

--- src/controllers/company.controllers.js
+++ src/controllers/company.controllers.js
@@ -2,37 +2,86 @@ const companyServices = require("../services/company.services");
 
 // Create account function
 const createAccount = async (req, res) => {
-  const data = await companyServices.createCompany(req.body);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.createCompany(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
+
 //create admin function
 const createAdmin = async (req, res) => {
-  const data = await companyServices.createAdmin(req.body);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.createAdmin(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
 
 //createStaff function
 const createStaff = async (req, res) => {
-  const data = await companyServices.createStaff(req.body);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.createStaff(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
 
 //login function
 const login = async (req, res) => {
-  const data = await companyServices.login(req.body);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.login(req.body);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
 
 //get companies function
 const getAllCompanies = async (req, res) => {
-  const data = await companyServices.getAllCompanies();
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.getAllCompanies();
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
 
 //search staff
 const searchStaff = async (req, res) => {
-  const data = await companyServices.searchStaff(req.query);
-  res.status(data.statusCode).json(data);
+  try {
+    const data = await companyServices.searchStaff(req.query);
+    res.status(data.statusCode).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      status: "failure",
+      statusCode: 500,
+    });
+  }
 };
 
 //forgot password function
