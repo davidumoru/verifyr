069719b19adb619f57df6171f3930a227c074c47
David Umoru <umorudavido@gmail.com>
2023-08-04 09:13:18 +0100

    Add fields to store updated payment transaction

--- src/models/payment.models.js
+++ src/models/payment.models.js
@@ -1,22 +1,46 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-  email: {
-    type: String,
+const paymentSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    date: {
+      type: String,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+    reference: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    ipAddress: {
+      type: String,
+    },
+    currency: {
+      type: String,
+    },
+    channel: {
+      type: String,
+    },
+    transactionId: {
+      type: Number,
+    },
+    paidAt: {
+      type: Date,
+    },
   },
-  amount: {
-    type: String,
-  },
-  date: {
-    type: String,
-  },
-  status: {
-    type: String,
-    default: "pending",
-  },
-  reference: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("payment", paymentSchema);
