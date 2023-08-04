be5a416f27e5258c695fba598fd9e5bc0194e539
David Umoru <umorudavido@gmail.com>
2023-08-04 09:11:09 +0100

    Edit initiate payment controller

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -4,9 +4,12 @@ const paymentServices = require("../services/payment.services");
 const initiatePayment = async (req, res) => {
   try {
     const response = await paymentServices.initiatePayment(req.user);
-    res.status(200).json(response);
+    res.status(response.statusCode).json(response);
   } catch (error) {
-    res.status(500).json(error);
+    return res.status(500).json({
+      message: "Unable to make payment",
+      status: "failure",
+    });
   }
 };
 
