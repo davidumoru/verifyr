2166d280219aa752d5a91e5da890f70cb130a705
David Umoru <umorudavido@gmail.com>
2024-06-24 20:54:01 +0100

    Implement user signup and login

---
+++ server/src/models/user.models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetPin: {
+    type: String,
+    default: null,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
