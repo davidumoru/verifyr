8240ba358455cc8d9409c537cd255bcd6d2aa9d8
David Umoru <umorudavido@gmail.com>
2024-06-24 21:29:40 +0100

    Refactor user.services

--- server/src/services/user.services.js
+++ server/src/services/user.services.js
@@ -1,21 +1,9 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const Joi = require("joi");
 const User = require("../models/user.models");
 const response = require("../utils/response");
-
-// Define Joi schemas
-const signupSchema = Joi.object({
-  firstName: Joi.string().min(1).required(),
-  lastName: Joi.string().min(1).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
+const signupSchema = require("../validations/signupSchema");
+const loginSchema = require("../validations/loginSchema");
 
 const signup = async (payload) => {
   try {
